86936ee7673ec79b731f7ad5d17e781d1d8935e8
casanders221418 <143522971+casanders221418@users.noreply.github.com>
2023-12-05 23:49:58 +0000

    slick demo

---
+++ slick-demo/assets/js/scripts.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+
+  $('.fade').slick({
+    dots: true,
+    infinite: false,
+    speed: 500,
+    fade: false,
+    slide: 'div',
+    cssEase: 'linear',
+    autoplay: true,
+    autoplaySpeed: 1000
+  });
+
+
+});
